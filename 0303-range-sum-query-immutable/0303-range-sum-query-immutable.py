f28b44ffe04b40a9318fb16310ee5533c1fc69de
1507kaushik-kk <1507kaushik@gmail.com>
2026-09-21 00:17:27 +0530

    Time: 2645 ms (11.35%), Space: 22.7 MB (64.51%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,13 +1,11 @@
 class NumArray:
-
-    def __init__(self, nums):
-        self.prefix = [0]
-
-        for num in nums:
-            self.prefix.append(self.prefix[-1] + num)
-
+    def __init__(self, nums): 
+        self.nums = nums
     def sumRange(self, left, right):
-        return self.prefix[right + 1] - self.prefix[left]
-        
+        total = 0
+        for i in range(left, right + 1): 
+            total += self.nums[i]
+        return total
+
 
 
